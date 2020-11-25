734b5b29f8c2aff47c66c1b539ab932992825742
Dhiraj Beri <46566839+dhirajberi@users.noreply.github.com>
2020-11-25 23:50:52 +0530

    Create 1-brand-name-generator.py

---
+++ 1-brand-name-generator.py
@@ -0,0 +1,12 @@
+#1. Create a greeting for your program.
+print("DAY 1 - Welcome to brand name generator tool")
+
+#2. Ask the user for the city that they grew up in.
+city = input('city?\n')
+#3. Ask the user for the name of a pet.
+pet = input('pet?\n')
+#4. Combine the name of their city and pet and show them their band name.
+print(f'{city} {pet}')
+#5. Make sure the input cursor shows on a new line
+
+#DAY 1 COMPLETED.
